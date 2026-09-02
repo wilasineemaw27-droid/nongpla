d528826e32ec27cfd37803f008e22d22d22d204f
wilasineemaw27-droid <wilasineemaw27@gmail.com>
2026-09-02 11:30:42 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbxba2Enfzaz4wu7supLpkW2V-3aQLk4AG6KtPjc6GczcifG4J7wXpcE85pmAZsoKXdBhg/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbz8Au9xTijmDWLLJ_mgZwMRVif69r_RDckh1414NNfGRhVwJC7uITBhIPs3irJfT_Sc_Q/exec'
   });
 })();
