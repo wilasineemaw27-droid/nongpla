9c1524cfcc0b3d8c26a9d7f98dc9f31bcd7463c5
wilasineemaw27-droid <wilasineemaw27@gmail.com>
2026-09-02 11:44:48 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbzHb89S4R0KqWCsprBL0DzzG5u37k0gFrBfZhtMujGnNIf5aFFeS7zNqV58Q-6lHAOYOw/exec';
